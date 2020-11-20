187c816af646b6cdc93ac42b868d46dbb0dc0576
dawitrash <73241300+dawitrash@users.noreply.github.com>
2020-11-20 13:35:50 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,60 +8,62 @@
 
 /*
 	Asigna la clase correspondiente en base a criterios de texto en cada fila
+	Las clases son de W3CSS, un framework con colores ya asignados a las mismas
+	
+	Se ha importado <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
 */
 
 function coloringGlpi() {
 
-	var rows = document.getElementsByTagName("tr")
+	// Modify row background and category colors
+	const rows = document.getElementsByTagName("tr")
 
 	for (let i = 0; i < rows.length; i++) {
 		const row = rows[i]
 
 		/*
-			NOTIFICACIONES: IPERIUS Y MOSY
+			FILAS: IPERIUS Y MOSY
 		*/
-		// Errores Iperius backup - red
+		// Errores Iperius backup
 		if ( row.innerHTML.includes("Copia de seguridad finalizada con errores") ) {
-			row.className += " glpi-iperius"
+			row.className += " w3-pale-red"
 		}
 		if ( row.innerHTML.includes("Copia de seguridad finalizada con avisos") ) {
-			row.className += " glpi-iperius"
+			row.className += " w3-pale-red"
 		}
-		// MOSY resolved - green
+		// MOSY problem resolved
 		if ( row.innerHTML.includes("Resolved") ) {
-			row.className += " glpi-mosyResolved"
+			row.className += " w3-pale-green"
 		}
-		// MOSY error average - orange
+		// MOSY error average
 		if ( row.innerHTML.includes("MOSY Average") ) {
-			row.className += " glpi-mosyOrange"
+			row.className += " w3-pale-yellow"
 		}
-		// MOSY high or disaster - black
-		if ( row.innerHTML.includes("MOSY High") ) {
-			row.className += " glpi-mosyRed"
+		// MOSY error high or disaster
+		if ( row.innerHTML.includes("MOSY High") || row.innerHTML.includes("MOSY Disaster") ) {
+			row.className += " w3-black"
 		}
-		if ( row.innerHTML.includes("MOSY Disaster") ) {
-			row.className += " glpi-mosyRed"
-		}
-
+		
 		/*
 			CATEGORÍAS: AUREA, MOSY/IPERIUS, VOIP
 		*/
 		// AUREA
 		if ( row.innerHTML.includes("Root entity &gt; NBDT &gt; AURE") ) {
-			row.className += " glpi-cat-aurea"
+			row.firstElementChild.className += " w3-blue"
 		}
 		// MOSY / IPERIUS
-		if ( row.innerHTML.includes("Root entity &gt; NBDT &gt; MOSY") ) {
-			row.className += " glpi-cat-mosyiperius"
-		}
-		if ( row.innerHTML.includes("Root entity &gt; NBDT &gt; IPERIUS") ) {
-			row.className += " glpi-cat-mosyiperius"
+		if ( row.innerHTML.includes("Root entity &gt; NBDT &gt; MOSY") ||  row.innerHTML.includes("Root entity &gt; NBDT &gt; IPERIUS") ) {
+			row.firstElementChild.className += " w3-red"
 		}
 		// VOIP
 		if ( row.innerHTML.includes("Root entity &gt; NBDT &gt; VOIP") ) {
-			row.className += " glpi-cat-voip"
+			row.firstElementChild.className += " w3-yellow"
 		}
 	}
 
+	const tables = document.getElementsByClassName("tab_cadrehov")
+	
+	console.log(tables[0])
+	tables[0].className += " w3-table w3-small w3-card-4"
 }
 coloringGlpi()
